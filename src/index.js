310a2b8acf1d345727d36adf52c9a7454d68409e
PavlenkoBohdan_W_PC <povlenkobohdan@gmail.com>
2017-11-21 16:20:12 +0200

    Redux React 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+//
+// ReactDOM.render(
+//     <App />
+//     , document.getElementById('root')
+// );
+import {createStore} from 'redux';
+function playlist(state= [],action){
+    if(action.type ==='ADD_TRACK'){
+        return [
+            ...state,
+            action.payload
+        ];
+    }
+    return state
+}
+const store =createStore(playlist);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+store.subscribe(()=>{
+   console.log('subscribe',store.getState());
+});
+
+store.dispatch({type: 'ADD_TRACK', payload:'Smells like spirit'});
+store.dispatch({type: 'ADD_TRACK', payload:'Enter sandmen'});
