80eb6c708945f641055a4a30485764cff851e450
PavlenkoBohdan_W_PC <povlenkobohdan@gmail.com>
2017-11-22 17:14:08 +0200

    after Redux React 4

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 class App extends Component {
+    addTrack() {
+        let trackValue = this.trackInput.value;
+        console.log('Add track', trackValue);
+        this.props.onAddTrack(trackValue);
+        this.trackInput.value='';
+    }
 
     render() {
         console.log(this.props.testStore);
         return (
             <div>
-                <input type="text" placeholder="Track"/>
-                    <button>Add</button>
-                    <ul>
-                        {this.props.testStore.map((track,index)=>
-                            <li key={index}>{track}</li>
-                        )}
-                    </ul>
+                <input ref={(input) => {
+                    this.trackInput = input
+                }} type="text" placeholder="Track"/>
+                <button onClick={this.addTrack.bind(this)}>Add track</button>
+                <ul>
+                    {this.props.testStore.map((track, index) =>
+                        <li key={index}>{track}</li>
+                    )}
+                </ul>
             </div>
         )
     }
@@ -22,7 +30,11 @@ class App extends Component {
 
 export default connect(
     state => ({
-        testStore:state
+        testStore: state
     }),
-    dispatch => ({})
+    dispatch => ({
+        onAddTrack: (trackName) => {
+            dispatch({type: 'ADD_TRACK', payload: trackName});
+        }
+    })
 )(App);
