06ba61e60b3c496f47ed4849b6b7e247d5fe8ad2
PavlenkoBohdan_W_PC <povlenkobohdan@gmail.com>
2017-11-22 15:59:01 +0200

    after Redux React 2

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@
 //     , document.getElementById('root')
 // );
 import {createStore} from 'redux';
-function playlist(state= [],action){
-    if(action.type ==='ADD_TRACK'){
+
+function playlist(state = [], action) {
+    if (action.type === 'ADD_TRACK') {
         return [
             ...state,
             action.payload
@@ -17,11 +18,25 @@ function playlist(state= [],action){
     }
     return state
 }
-const store =createStore(playlist);
 
-store.subscribe(()=>{
-   console.log('subscribe',store.getState());
+const store = createStore(playlist);
+
+const addTrackBtn = document.querySelectorAll('.addTrack')[0];
+const trackInput = document.querySelectorAll('.trackInput')[0];
+const list = document.querySelectorAll('.list')[0];
+
+store.subscribe(() => {
+    list.innerHTML = '';
+    trackInput.value = '';
+    store.getState().forEach(track => {
+        const li = document.createElement('li');
+        li.textContent = track;
+        list.appendChild(li);
+    })
 });
 
-store.dispatch({type: 'ADD_TRACK', payload:'Smells like spirit'});
-store.dispatch({type: 'ADD_TRACK', payload:'Enter sandmen'});
+
+addTrackBtn.addEventListener('click', () => {
+    const trackName = trackInput.value;
+    store.dispatch({type: 'ADD_TRACK', payload: trackName});
+});
