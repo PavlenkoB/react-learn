145c29f049d429105bf197ae6e40777886b2c028
PavlenkoBohdan_W_PC <povlenkobohdan@gmail.com>
2018-01-16 16:05:20 +0200

    add example todo list

--- src/index.js
+++ src/index.js
@@ -1,66 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import './index.css';
-import App from './App';
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
+let store = createStore(todoApp)
 
-const initialState=[
-    'Smell like spirit',
-    'Enter Sandmen'
-];
-
-function playlist(state = initialState, action) {
-    if (action.type === 'ADD_TRACK') {
-        return [
-            ...state,
-            action.payload
-        ];
-    }
-    return state
-}
-
-const store = createStore(playlist, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-ReactDOM.render(
+render(
     <Provider store={store}>
         <App/>
-    </Provider>
-    , document.getElementById('root')
-);
-
-
-// import {createStore} from 'redux';
-//
-// function playlist(state = [], action) {
-//     if (action.type === 'ADD_TRACK') {
-//         return [
-//             ...state,
-//             action.payload
-//         ];
-//     }
-//     return state
-// }
-//
-// const store = createStore(playlist);
-//
-// const addTrackBtn = document.querySelectorAll('.addTrack')[0];
-// const trackInput = document.querySelectorAll('.trackInput')[0];
-// const list = document.querySelectorAll('.list')[0];
-//
-// store.subscribe(() => {
-//     list.innerHTML = '';
-//     trackInput.value = '';
-//     store.getState().forEach(track => {
-//         const li = document.createElement('li');
-//         li.textContent = track;
-//         list.appendChild(li);
-//     })
-// });
-//
-//
-// addTrackBtn.addEventListener('click', () => {
-//     const trackName = trackInput.value;
-//     store.dispatch({type: 'ADD_TRACK', payload: trackName});
-// });
+    </Provider>,
+    document.getElementById('root')
+)
