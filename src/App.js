4dbac99083379fb473f1ab1cd328065d70764619
Bohdan_pavlenko <godexxxx@gmail.com>
2024-10-10 16:38:06 +0300

    update config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
-
 function App() {
-  return (
-    <div>
-      <h1>Start</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Start</h1>
+            <p>hi mister</p>
+        </div>
+    );
 }
 
 export default App;
