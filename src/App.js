7132ae64046aa1287737eaceb01c10d3f3776581
Bohdan_pavlenko <godexxxx@gmail.com>
2024-10-11 11:43:07 +0300

    end of 21

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import CostItem from "./components/CostItem";
 
 function App() {
+
+    const costs = [
+        {
+            date: new Date(2021, 2, 12),
+            description: 'Freezer',
+            amount: 999.99
+        },
+        {
+            date: new Date(2021, 11, 15),
+            description: 'Mac',
+            amount: 2000.99
+        },
+        {
+            date: new Date(2021, 3, 1),
+            description: 'coat',
+            amount: 100.99
+        }
+    ]
     return (
         <div>
             <h1>Start</h1>
-            <CostItem />
+            <CostItem
+                date={costs[0].date}
+                description={costs[0].description}
+                amount={costs[0].amount}
+            ></CostItem>
         </div>
     );
 }
