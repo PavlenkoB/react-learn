eb3ca943017be72e46e14275b585eab262c4acb0
Bohdan_pavlenko <godexxxx@gmail.com>
2024-10-11 11:50:42 +0300

    end of 22

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -2,9 +2,16 @@ import './CostItem.css'
 
 function CostItem(props) {
 
+    let month = props.date.toLocaleString('ua-UA', {month: "long"});
+    let year = props.date.getFullYear()
+    let day = props.date.toLocaleString('ua-UA', {day: "2-digit"});
     return (
         <div className='cost-item'>
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className='cost-item__description'>
                 <h2>{props.description}</h2>
                 <div className='cost-item__price'>
